e7d6ee761f1aff80c6a3de0d7316a118f4b62205
Davis Walsh <theDavis@users.noreply.github.com>
2018-03-11 17:28:06 -0400

    simplify.

--- src/io/github/thedavis/chip8/Chip8.java
+++ src/io/github/thedavis/chip8/Chip8.java
@@ -1,13 +1,19 @@
 package io.github.thedavis.chip8;
 
+import java.util.Map;
+import java.util.Optional;
+
 public class Chip8 {
+
+    private final boolean debug;
 
     private final CPU cpu;
     private final Graphics graphics;
     private final Sound sound;
     private final InputDevice inputDevice;
 
-    public Chip8(){
+    public Chip8(boolean debug){
+        this.debug = debug;
         this.cpu = new CPU();
         this.graphics = new Graphics();
         this.sound = new Sound();
@@ -15,22 +21,21 @@ public class Chip8 {
     }
 
     public void initialize(){
-        //TODO: implement
+        //TODO: implement intializing components
     }
 
     public void loadRom(){
-        //TODO: implement
+        //TODO: implement loading rom
+    }
+
+    public void run(){
+        //TODO: implement main loop
     }
 
     public static void main(String[] args){
-
-        Chip8 chip8 = new Chip8();
+        Chip8 chip8 = new Chip8(false);
         chip8.initialize();
-
         chip8.loadRom();
-    }
-
-    public static Map<String,String> parseArgs(String[] args){
-
+        chip8.run();
     }
 }
